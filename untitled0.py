2c7587241a4b601950f926d836109c2b7cdac8f1
feizei2008 <zacklike@hotmail.com>
2017-05-07 14:33:44 +0800

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 07 14:24:56 2017
+
+@author: zack zhang
+"""
+
+x = [1,2,3,4,5,6]
+print x[::2]#第一个：代表start，第二个：代表end，2代表step
+print x[1::2]#1代表start的index，两个：代表到end，2代表step
